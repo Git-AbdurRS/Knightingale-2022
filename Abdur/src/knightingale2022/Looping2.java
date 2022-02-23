4edf723ffdbe50ac34d124de4916936494fc2fa7
abdur <abdur@LAPTOP-HVNS8L9N>
2022-02-24 00:53:43 +0530

    Knightingale2022

---
+++ Abdur/src/knightingale2022/Looping2.java
@@ -0,0 +1,16 @@
+package knightingale2022;
+
+public class Looping2 {
+
+	public static void main(String[] args) {
+//		for(;;) {
+//		System.out.println("infinite loop");
+//	}
+	
+	for(int i=10;i>0;i--) {
+		System.out.println("the loop executes...:"+i);
+	}
+
+	}
+
+}
